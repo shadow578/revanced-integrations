cfc2a79fc2b23eba3023b68fc165bae0e22582f6
shadow578 <52449218+shadow578@users.noreply.github.com>
2022-08-10 12:11:58 +0200

    check for microG on startup

--- app/src/main/java/app/revanced/integrations/ReVancedApp.kt
+++ app/src/main/java/app/revanced/integrations/ReVancedApp.kt
@@ -1,6 +1,8 @@
 package app.revanced.integrations
 
 import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.Toast
 import com.google.android.apps.youtube.app.YouTubeTikTokRoot_Application
 
 /**
@@ -14,9 +16,42 @@ class ReVancedApp : YouTubeTikTokRoot_Application() {
     override fun onCreate() {
         instance = this
         super.onCreate()
+        checkMicroG()
+    }
+
+    /**
+     * check if microG is installed.
+     * if the microG patch is not active, this will do nothing
+     */
+    private fun checkMicroG() {
+        // get microG package name
+        // if not set, assume microG patch is not active
+        val metaData =
+            packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA).metaData
+        val microGPackageName = metaData.getString(META_MICROG_PACKAGE_NAME)
+        if (microGPackageName.isNullOrEmpty()) {
+            return
+        }
+
+        // check if microG is installed
+        try {
+            packageManager.getPackageInfo(microGPackageName, PackageManager.GET_ACTIVITIES)
+            return
+        } catch (_: PackageManager.NameNotFoundException) {
+            // not installed
+        }
+
+        // show info that microG is not installed
+        Toast.makeText(
+            this,
+            "ReVanced microG was not found! The app may not work as expected.",
+            Toast.LENGTH_LONG
+        ).show()
     }
 
     companion object {
+        const val META_MICROG_PACKAGE_NAME = "app.revanced.android.youtube.MICROG_PACKAGE_NAME"
+
         @Volatile
         private lateinit var instance: ReVancedApp
 
